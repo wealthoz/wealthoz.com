950ce62cc54905778dd6c197159f076d9c34917e
Ivan Petkov <ivan.v.petkov@gmail.com>
2014-02-05 14:21:46 +0200

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   
+  has_secure_password
   before_save { self.email = email.downcase }
   
   validates :name,  presence: true, length: { maximum: 50 }
@@ -8,7 +9,6 @@ class User < ActiveRecord::Base
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
                     
-  has_secure_password
   validates :password, length: { minimum: 6 }
 
 end
