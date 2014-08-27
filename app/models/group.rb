356ceda9ecd21ca0058a24e869d77f130085e76f
Georgi Tapalilov <tapalilov@gmail.com>
2014-08-27 09:08:23 +0300

    [model] group user assoc

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,15 +1,20 @@
 class Group < ActiveRecord::Base
   belongs_to :fx
   has_one  :fs
-  has_many :users, dependent: :destroy
+  has_many :group_users
+  has_many :users, through: :group_users
   has_many :projects, dependent: :destroy
   has_many :accounts
   has_many :ledgers
   has_many :budgets
-  
-  validates :name, presence: true, 
-                   length: { maximum: 25 }, 
+
+  validates :name, presence: true,
+                   length: { maximum: 25 },
                    uniqueness: { case_sensitive: false }
-                   
+
   default_scope -> { order('name DESC') }
+
+  def to_s
+    name
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-  belongs_to :group
+  has_one :group_user
+  has_one :group, through: :group_user
 
-  has_many :microposts, dependent: :destroy  
+  has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
   has_many :reverse_relationships, foreign_key: "followed_id",
@@ -11,15 +12,15 @@ class User < ActiveRecord::Base
 
   has_secure_password
   before_save { self.email = email.downcase }
-  
+
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-                    
+
   validates :password, length: { minimum: 6 }
-  
+
   def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
@@ -27,14 +28,14 @@ class User < ActiveRecord::Base
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
-  
+
    def feed
     # This is preliminary. See "Following users" for the full implementation.
     #Micropost.where("user_id = ?", id)
     Micropost.from_users_followed_by(self)
 
   end
-  
+
    def following?(other_user)
     relationships.find_by(followed_id: other_user.id)
   end
@@ -42,7 +43,7 @@ class User < ActiveRecord::Base
   def follow!(other_user)
     self.relationships.create!(followed_id: other_user.id)
   end
-  
+
   def unfollow!(other_user)
     relationships.find_by(followed_id: other_user.id).destroy
   end
@@ -54,7 +55,7 @@ class User < ActiveRecord::Base
       self.remember_token = User.encrypt(User.new_remember_token)
     end
 
- 
-  
+
+
 
 end
